f8c3881dc3072dfdcc5753fe48441975923b6779
mayuri19922 <mayurigupta19922@gmail.com>
2019-09-20 17:01:40 +0530

    changes in dashboard

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -10,6 +10,7 @@ import { Observable } from 'rxjs';
 export class TaskService {
 
 	public products: any;
+	public arrays: any;
 
 	constructor(private httpClient : HttpClient) { }
 
@@ -27,22 +28,48 @@ export class TaskService {
 			PlannedOrderNo: '415654'
 		}])};
 		this.httpClient.post("http://172.16.6.117/OptiProRCCPGanttChart/RCCPGanttChart/GetGanttChartData",jObject,this.httpOptions).subscribe((res : any[])=>{
-			this.products = res;
-			/*for(let i=0; i<this.products.length; i++){
-				//var hours = Math.abs(this.products[i].STARTDATE - this.products[i].ENDDATE) / 36e5;
-				//this.products[i]["start_date"] = hours;
-				this.products[i]["duration"] = this.products[i].DURATION;
-			}
-			console.log(this.products);*/
-		});
+            this.products = res.map(function(obj) { 
+                obj['id'] = obj['SeqNo'];
+                delete obj['SeqNo'];
+
+                obj['text'] = obj['name'];
+                delete obj['name'];
+
+                obj['start_date'] = obj['STARTDATETIME']; 
+                delete obj['STARTDATETIME'];
+                 
+                obj['end_date'] = obj['ENDDATETIME']; 
+                delete obj['ENDDATETIME']; 
+
+                obj['progress'] = 1; 
+
+                obj['parent'] = obj['ParantId']; 
+                delete obj['ParantId'];
+
+                obj['open'] = true; 
+
+                if(obj['parent'] == ""){
+                    obj['type'] = "operation"; 
+                }else{
+                    obj['type'] = "resource";  
+                }
+
+                delete obj['OPTM_OPERNO'];
+                delete obj['OPTM_OPR_ID'];
+                delete obj['OPTM_RES_ID']
+                delete obj['U_O_RESNAME']
+                
+                return obj; 
+            }); 
+            localStorage.setItem('ganttChart', JSON.stringify(this.products));
+        });
+
+        this.arrays = JSON.parse(localStorage.getItem('ganttChart'));
+        this.products = this.arrays; 
+        //console.log(JSON.stringify(this.products));
 
 		return Promise.resolve(
-			 [{id: 1, text: "Operation #1", start_date: "2019-09-15 09:18 ", description:"demo", end_date:"2019-09-18 15:30", progress: 0.45, parent: "", open:true, type:'operation'},
-             {id: 2, text: "Resource #1-1", start_date: "2019-09-15 09:18", description:"demo", end_date:"2019-09-18 15:30", progress: 0.45, parent: "1", open:true, type:'resource' },
-             {id: 3, text: "Resource #1-2", start_date: "2019-09-16 10:54", description:"demo", end_date:"2019-09-18 15:30", progress: 0.45, parent: "1", open:true, type:'resource'},
-             {id: 4, text:"Operation #2", start_date:"2019-09-17 15:29", description:"demo", end_date:"2019-09-19 15:30", progress:0.6, parent: "", open: true, type:'operation'},
-             {id: 5, text:"Resource #2-1", start_date:"2019-09-17 15:29 ", description:"demo", end_date:"2019-09-18 15:30", progress:0.6, parent:"4", open: true, type:'resource' },
-             {id: 6, text:"Resource #2-2", start_date:"2019-09-18 11:17", description:"demo", end_date:"2019-09-21 15:30", progress:0.6, parent:"4", open: true, type:'resource'}]
+			this.products
 		);
 	}
 }	
